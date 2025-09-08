8fa1d2d3eebcaa7c65470257ca3d2d245dc3e5f8
sohaib <rajashahzaibabdullah8467@gmail.com>
2025-09-08 20:10:21 +0500

    Add classes to buttons and functions

--- script.js
+++ script.js
@@ -1,30 +1,85 @@
-const add = function(num1, num2) {
+// calculator functions
+const add = function (num1, num2) {
     return num1 + num2;
 }
 
-const subtract = function(num1, num2) {
+const subtract = function (num1, num2) {
     return num1 - num2;
 }
 
-const multiply = function(num1, num2) {
+const multiply = function (num1, num2) {
     return num1 * num2;
 }
 
-const divide = function(num1, num2) {
+const divide = function (num1, num2) {
     return num1 / num2;
 }
 
-const square = function(num1) {
+const square = function (num1) {
     return num1 ** 2;
 }
 
 
-let firstNumber;
-let secondNUmber;
-let operator;
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let pressed = false;
 
-const operate = function(num1, num2, operand) {
+
+const operate = function (num1, num2, operand) {
     return operand(num1, num2);
 }
 
-console.log(operate(2, 3, add))
+
+const AC = function () {
+    const clear = document.querySelector(".clear");
+    clear.addEventListener("click", (event) => {
+        const answer = document.querySelector(".answer");
+        answer.textContent = "0";
+        const expression = document.querySelector(".expression");
+        expression.textContent = "";
+        firstNumber = 0;
+        secondNumber = 0;
+        operator = "";
+    })
+}
+
+
+const operatorChosen = function () {
+    return (operator) ? true : false;
+}
+
+
+const readyToCalculate = function () {
+    return (pressed) ? true : false;
+}
+
+
+const equalListener = function () {
+    const equal = document.querySelector(".equal");
+    equal.addEventListener("click", (event) => {
+        if (firstNumber && operator && secondNumber) {
+            pressed = true;
+        }
+    })
+}
+
+
+const appendFirstNumber = function (value) {
+    firstNumber += toString(value);
+}
+
+
+const appendSecondNumber = function (value) {
+    secondNumber += toString(value);
+}
+
+const firstToFloat = function () {
+    firstNumber = parseFloat(firstNumber);
+}
+
+const secondToFloat = function () {
+    secondNumber = parseFloat(secondNumber);
+}
+
+
